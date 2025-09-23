b6d4134dd72cc476c6b2f7fccfbcbddae76151ea
hiagosz901 <hiago3259@gmail.com>
2025-09-23 20:22:27 -0300

    php

---
+++ array.php
@@ -0,0 +1,51 @@
+<?php 
+	/*
+	$aluno = array();
+
+	$aluno[0]="Maria";
+	$aluno[1]="Josephe";
+	$aluno[2]="Alana";
+	$aluno[3]="Sebastian";
+
+	echo ($aluno[0] . "<br/>" . $valor[1]);
+	
+$nomes = array("Maria", "Josephe","Alana");
+
+$idade[0] = 20;
+$idade[1] = 10;
+$idade[2] = 15;
+
+echo("<br/>");
+
+echo($nomes[0] . "<br/>".  $nomes[1] . "<br/>". $nomes[2]);
+
+$email[] = "Maria@hotmail.com";
+$email[] = "Josephe@outlook.com";
+$email[] = "Alana@gmail.com";
+
+
+echo("<br/>");
+echo("<pre>");
+print_r($email);
+echo("<br/>");
+var_dump($email);
+echo("</pre>");
+
+
+$aluno = array("Osvaldo", 26, "osvaldo@gmail.com", 25.5, true);
+
+echo("<pre>");
+var_dump($aluno);
+echo("</pre>");
+echo("<br/>");
+echo("<pre>");
+var_dump($aluno);
+echo("</pre>");
+*/
+
+for ($i=0 ; < 6 ; $i++ ){
+	$vetor[$i] = 12;
+}
+for ($i=0 ; < 6 ; $i++){
+	echo "O valor do vetor:" . $vetor[$i] . "<br/>";
+}
